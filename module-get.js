cfd0b43135309098b0ed537d04cc86ce93aef096
Max <masamune.code@gmail.com>
2021-03-26 14:00:40 +0100

    ✨MOD: module-get > manage all file types

--- module-get.js
+++ module-get.js
@@ -1,9 +1,17 @@
 // On passe une url, si tu trouves le fichier côté serveur tu le renvoie direct au http
 //      cf. 04-ma-premiere-ES-asynchrone-correction.js
-const fs = require('fs');
+const
+    fileExtToContentType    = require('./fileExtToContentType')
+    ,fs                     = require('fs')
+    ,path                   = require('path')
+;
 
 function gonnaGetGet(urlFournie, responseFournieParHttp) {
     console.log(`gonnaGetGet(${urlFournie})`);
+
+    // console.log(path.extname(urlFournie));
+    const lExtensionDufichier   = path.extname(urlFournie);
+    const contentType           = fileExtToContentType(lExtensionDufichier);
 
     // Asyc, du coup pas de return > on passe par un callback
     //      https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
@@ -12,12 +20,15 @@ function gonnaGetGet(urlFournie, responseFournieParHttp) {
         'utf8', // Force String output
         (err, data) => {
             console.log("fs.readFile() > err :", err);
-            console.log("fs.readFile() > data :", data);
-        
+            // Voir les données passer en brut
+            //      ATTENTION, les console log ralentissent les perfs x)
+            // console.log("fs.readFile() > data :", data);
+            
+            // TODO: Gérer ici les 404 et autres joyeusetées?
             if (err)
                 throw err;
             
-            responseFournieParHttp.writeHead(200, { 'Content-Type': 'text/html' });
+            responseFournieParHttp.writeHead(200, { 'Content-Type': contentType });
             // Correction du chemin sur le serveur
             return responseFournieParHttp.end(data);
             
